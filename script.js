e35669ee03cbd524c9b71369498a1391646732f6
Manamana <allentrobson@gmail.com>
2022-08-01 14:51:37 -0600

    Functions to check whether prompts are correct

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    
+
   var password = generatePassword();
 
 
@@ -17,23 +17,75 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-function generatePassword(){
+// Asks for various prompts to creat the password 
+function generatePassword() {
 
   var randPassword;
 
-    let passLength = prompt("Between 8 and 128 characters, how long would you like your password to be?");
-
-    let passLowerCase = prompt("Would you like to use lowercase letters?");
-
-    let passUpperCase = prompt("Would you like to use uppercase letters?");
-
-    let numeric = prompt("Would you like to use numbers?");
-
-    let specialChar = prompt("Would you like to use special characters?");
 
 
+  let passLength = prompt("Between 8 and 128 characters, how long would you like your password to be?");
 
-return sign;
+  // Only accepts correct prompt inputs 
+  while (isCorrectPrompt(passLength) === false) {
 
+    passLength = prompt("That was an incorrect input, please try again." + "\r\n" + "\r\n" + "Between 8 and 128 characters, how long would you like your password to be?");
+
+  };
+
+
+
+
+  let passLowerCase = prompt("Would you like to use lowercase letters?" + "\r\n" + "\r\n" + "Please type 'yes' or 'no'.").toLowerCase();
+
+  console.log(passLowerCase);
+
+  while (isCorrectPromptYesNo(passLowerCase) === false) {
+
+    passLowerCase = prompt("That was an incorrect input, please try again." + "\r\n" + "\r\n" + "Would you like to use lowercase letters?" + "\r\n" + "\r\n" + "Please type 'yes' or 'no'.").toLowerCase();
+
+  };
+
+
+
+  let passUpperCase = prompt("Would you like to use uppercase letters?");
+
+  let numeric = prompt("Would you like to use numbers?");
+
+  let specialChar = prompt("Would you like to use special characters?");
+
+
+
+  return randPassword;
+
+}
+
+
+// Checks to see if the length prompt is within acceptable bounds and ends if the user presses cancel 
+function isCorrectPrompt(x) {
+  if (x === null) {
+    generatePassword.end;
+
+  } else if (x >= 8 && x <= 128) {
+    return true;
+
+
+  } else {
+    return false;
+  }
+}
+
+// Checks to see if the prompt is yes or no 
+function isCorrectPromptYesNo(y) {
+  console.log(y);
+  if (y === null) {
+    generatePassword.end;
+
+  } else if (y ==="yes" || y==="no") {
+    return true;
+
+
+  } else {
+    return false;
+  }
 }
